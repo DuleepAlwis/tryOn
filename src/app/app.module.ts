05f34da833f78cb2c801760beab42671894de9dc
DuleepAlwis <duleepalwis@gmail.com>
2019-03-15 17:11:21 +0530

    Login & Registration pages created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { RouterModule, Routes } from "@angular/router";
+import { RegisterComponent } from "./register/register.component";
+import { HomeNavigationComponent } from "./home-navigation/home-navigation.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    HomeNavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(
-      [
-        {
-          component : HomeComponent,
-          path : ''
-        }
-      ]
-    )
+    RouterModule.forRoot([
+      {
+        component: HomeComponent,
+        // tslint:disable-next-line:quotemark
+        path: ""
+      },
+      {
+        component: RegisterComponent,
+        path: "Register"
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
